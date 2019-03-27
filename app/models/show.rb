ef17f9883393ca2631594dfdb5e8dfc98011030f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-27 16:38:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def build_network(arg)
+  def build_association(arg)
     new_network = Network.new(arg)
     self.network_id = new_network
   end
