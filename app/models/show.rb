c9a7af85bca4a1ccd9cd8e565e64a1011c1a66b8
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-27 16:31:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,9 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(arg)
-    # binding.pry
     new_network = Network.new(arg)
     self.network_id = new_network
-# binding.pry
   end
 end
