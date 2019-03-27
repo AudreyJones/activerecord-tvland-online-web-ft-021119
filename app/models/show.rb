908fe39ca5e4071380b5cb00c5571fd7a9116ce4
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-27 16:29:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(arg)
-    arg = Network.new(arg)
+    new_network = Network.new(arg)
 binding.pry
   end
 end
